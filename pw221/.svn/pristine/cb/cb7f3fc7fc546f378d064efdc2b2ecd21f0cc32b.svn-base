26d77e669ef5385b90629372c2effeb7a50f603b
MarshallXXVI <Soonthornvech3@gmail.com>
2022-07-07 19:15:05 +0700

    up all

---
+++ pw221/.svn/pristine/cb/cb7f3fc7fc546f378d064efdc2b2ecd21f0cc32b.svn-base
@@ -0,0 +1,6 @@
+/* Copyright 2022 */
+//  MockTerminalManager.h
+//  Clang
+//
+//  Created by Paramie Willmann on 07.07.22.
+//
---
+++ pw221/blatt-10/MockTerminalManager.cpp
@@ -0,0 +1,6 @@
+/* Copyright 2022 */
+//  MockTerminalManager.cpp
+//  Clang
+//
+//  Created by Paramie Willmann on 07.07.22.
+//
