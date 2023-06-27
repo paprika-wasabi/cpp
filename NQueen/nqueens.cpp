41479b8c8b0d4054b687fd3ed859dcdb13dc8072
Paramie <paramie.willmann3@gmail.com>
2023-06-27 04:43:15 +0200

    a

--- NQueen/nqueens.cpp
+++ NQueen/nqueens.cpp
@@ -12,17 +12,16 @@ void NQueens::check_and_add_child(Node *parent, bool direction,
   // NB: this is just a helper function I found useful to have, but
   Board tempBoard;
     if (direction) {
-        tempBoard = parent->if_true->get_configuration();
+        tempBoard = *parent->if_true->get_configuration();
 
     } else {
-        tempBoard = parent->if_false->get_configuration();
+        tempBoard = *parent->if_false->get_configuration();
     }
 
     if (tempBoard.is_valid() or tempBoard.is_full()) {
         bdd.import_node(tempBoard);
     } else {
         bdd.import_node(tempBoard);
-
     }
 
 }
@@ -34,10 +33,10 @@ void NQueens::construct_bdd() {
   positionToProcess.push(rootNode);
 
   while (!positionToProcess.empty()) {
-    //Board currentBoard = positionToProcess.top();
-    //positionToProcess.pop();
-    //Board tempBoard = rootNode->if_true->get_configuration();
-    //check_and_add_child(rootNode, 0, positionToProcess);
-    //check_and_add_child(rootNode, 1, positionToProcess);
+    Board currentBoard = positionToProcess.top()->get_configuration();
+    positionToProcess.pop();
+    Board tempBoard = rootNode->if_true->get_configuration();
+    check_and_add_child(rootNode, 0, positionToProcess);
+    check_and_add_child(rootNode, 1, positionToProcess);
   }
 }
