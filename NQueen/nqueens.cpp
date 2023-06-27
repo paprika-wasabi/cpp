fa42a0200f03acb9ca1efcf4c0a4ba356eb1898a
Paramie <paramie.willmann3@gmail.com>
2023-06-27 05:26:16 +0200

    a

--- NQueen/nqueens.cpp
+++ NQueen/nqueens.cpp
@@ -7,21 +7,21 @@
 void NQueens::check_and_add_child(Node *parent, bool direction,
                                   Stack<Node *> stack) {
     Board tempBoard;
-    Node *tempNode;
+    //Node *tempNode;
     if (direction) {
         tempBoard = parent->if_true->get_configuration();
-        tempNode = parent->if_true;
+        //tempNode = parent->if_true;
     } else {
         tempBoard = parent->if_false->get_configuration();
-        tempNode = parent->if_false;
+        //tempNode = parent->if_false;
     }
 
     if (tempBoard.is_full() or !tempBoard.is_valid()) {
         bdd.import_node(tempBoard);
     } else {
         bdd.import_node(tempBoard);
-        stack.push(tempNode);
-        bdd.connect(parent, tempNode, direction);
+        //stack.push(tempNode);
+        //bdd.connect(parent, tempNode, direction);
     }
 
 }
@@ -36,6 +36,6 @@ void NQueens::construct_bdd() {
     Board currentBoard = positionToProcess.top()->get_configuration();
     positionToProcess.pop();
     check_and_add_child(rootNode, 0, positionToProcess);
-    //check_and_add_child(rootNode, 1, positionToProcess);
+    check_and_add_child(rootNode, 1, positionToProcess);
   }
 }
