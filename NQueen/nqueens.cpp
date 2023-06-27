d5f0e9a00089f40bb1e61efb453c36bfc2f0b0bf
Paramie <paramie.willmann3@gmail.com>
2023-06-27 06:14:33 +0200

    a

--- NQueen/nqueens.cpp
+++ NQueen/nqueens.cpp
@@ -6,22 +6,26 @@
 // node to the stack if needed
 void NQueens::check_and_add_child(Node *parent, bool direction,
                                   Stack<Node *> stack) {
-    Board tempBoard;
-    //Node *tempNode;
+    Board tempBoard(size);
+    Node *tempNode;
     if (direction) {
-        tempBoard = parent->if_true->get_configuration();
-        //tempNode = parent->if_true;
+        tempBoard.next(true);
+        *tempNode = bdd.import_node(tempBoard);
     } else {
-        tempBoard = parent->if_false->get_configuration();
-        //tempNode = parent->if_false;
+        tempBoard.next(false);
+        *tempNode = bdd.import_node(tempBoard);
     }
 
+
     if (tempBoard.is_full() or !tempBoard.is_valid()) {
-        bdd.import_node(tempBoard);
+        if (direction) {
+            bdd.connect_true(parent, direction);
+        } else {
+            bdd.connect_false(parent, direction);
+        }
     } else {
         bdd.import_node(tempBoard);
-        //stack.push(tempNode);
-        //bdd.connect(parent, tempNode, direction);
+        bdd.connect(parent, tempNode, direction);
     }
 
 }
@@ -29,13 +33,13 @@ void NQueens::check_and_add_child(Node *parent, bool direction,
 void NQueens::construct_bdd() {
   Stack<Node*> positionToProcess;
   Board initBoard(size);
-  Node *rootNode = new Node(0, initBoard);
+  Node *rootNode = bdd.import_node(initBoard);
   positionToProcess.push(rootNode);
 
   while (!positionToProcess.empty()) {
-    Board currentBoard = positionToProcess.top()->get_configuration();
+    Node *workingNode = positionToProcess.top();
     positionToProcess.pop();
-    check_and_add_child(rootNode, 0, positionToProcess);
-    check_and_add_child(rootNode, 1, positionToProcess);
+    check_and_add_child(workingNode, 0, positionToProcess);
+    check_and_add_child(workingNode, 1, positionToProcess);
   }
 }
