047fdd0f47338ea9cff895ac6bc74797f7d99528
Paramie <paramie.willmann3@gmail.com>
2023-06-27 04:56:30 +0200

    a

--- NQueen/nqueens.cpp
+++ NQueen/nqueens.cpp
@@ -9,6 +9,22 @@
 // node to the stack if needed
 void NQueens::check_and_add_child(Node *parent, bool direction,
                                   Stack<Node *> &stack) {
+    Board tempBoard;
+    Node *tempNode;
+    if (direction) {
+        tempBoard = parent->if_true->get_configuration();
+        tempNode = parent->if_true;
+    } else {
+        tempBoard = parent->if_false->get_configuration();
+        tempNode = parent->if_false;
+    }
+
+    if (tempBoard.is_full() or !tempBoard.is_valid()) {
+        bdd.import_node(tempBoard);
+    } else {
+        bdd.import_node(tempBoard);
+        &stack.push(tempNode);
+    }
 
 }
 
