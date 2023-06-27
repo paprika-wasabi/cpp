3d4eb9852cef25b56c6f01438399ef1184ac9d56
Paramie <paramie.willmann3@gmail.com>
2023-06-27 04:34:15 +0200

    a

--- NQueen/nqueens.cpp
+++ NQueen/nqueens.cpp
@@ -27,10 +27,10 @@ void NQueens::check_and_add_child(Node *parent, bool direction,
 }
 
 void NQueens::construct_bdd() {
-  std::stack<Node> positionToProcess;
+  std::stack<Node*> positionToProcess;
   Board initBoard(size);
-  Node *rootNode = new Node(0, initBoard);
-  positionToProcess.push(rootNode);
+  const Node* rootNode = new Node(0, initBoard);
+  positionToProcess.push(*rootNode);
 
   while (!positionToProcess.empty()) {
     //Board currentBoard = positionToProcess.top();
