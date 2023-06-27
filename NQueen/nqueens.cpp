1bc7590cf988205bcc94c65f26ed2d01702f8477
Paramie <paramie.willmann3@gmail.com>
2023-06-27 06:46:21 +0200

    a

--- NQueen/nqueens.cpp
+++ NQueen/nqueens.cpp
@@ -8,13 +8,13 @@ void NQueens::check_and_add_child(Node *parent, bool direction,
                                   Stack<Node *> stack) {
     //Board tempBoard(size);
     //Node tempNode;
-    if (direction) {
-        //tempBoard.next(true);
-        //tempNode = bdd.import_node(tempBoard);
-    } else {
-        //.next(false);
-        //tempNode = bdd.import_node(tempBoard);
-    }
+//    if (direction) {
+//        //tempBoard.next(true);
+//        //tempNode = bdd.import_node(tempBoard);
+//    } else {
+//        //.next(false);
+//        //tempNode = bdd.import_node(tempBoard);
+//    }
 
 
 //    if (tempBoard.is_full() or !tempBoard.is_valid()) {
